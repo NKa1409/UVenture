f6dfb1d364530a83498d3c552c42f39245b3521b
NKa1409 <96614861+NKa1409@users.noreply.github.com>
2025-07-18 09:34:22 +0200

    Update start_on_windows.bat

--- start_on_windows.bat
+++ start_on_windows.bat
@@ -36,17 +36,25 @@ FOR %%P IN ("%LocalAppData%\Programs\Python\Python3*\python.exe") DO (
     )
 )
 
+:: 3. Try local folder python312 in script directory
+echo Python not found in default user installation path. Checking in UVenture folder...
+SET "LOCAL_PYTHON=%~dp0python312\python.exe"
+IF EXIST "%LOCAL_PYTHON%" (
+    SET "PYTHON_EXEC=%LOCAL_PYTHON%"
+    goto :found_python
+)
+
 :: 3. Not found — install Python now
 echo Could not locate Python. Installing Python for current user...
 set "PYTHON_INSTALLER=python-installer.exe"
-powershell -Command "Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe -OutFile '%PYTHON_INSTALLER%'"
+powershell -Command "Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe -OutFile '%PYTHON_INSTALLER%'"
 :: Get path to current script directory
 set "SCRIPT_DIR=%~dp0"
 :: Set Python install directory to a subfolder "python312" inside the script's directory
 set "PYTHON_TARGET=%SCRIPT_DIR%python312"
 :: Ensure the folder exists
 mkdir "%PYTHON_TARGET%"
-start /wait "" "%PYTHON_INSTALLER%" /quiet InstallAllUsers=0 PrependPath=1 Include_test=0 TargetDir="%PYTHON_TARGET%"
+start /wait "" "%PYTHON_INSTALLER%" /quiet InstallAllUsers=0 PrependPath=0 Include_test=0 TargetDir="%PYTHON_TARGET%"
 del "%PYTHON_INSTALLER%"
 
 :found_python
